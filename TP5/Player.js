482fae1d48e01d0423d20185235cc22a1e27c4ed
MaximePetit03 <Maxime.petit@coda-student.school>
2026-01-13 13:59:30 +0100

    TP5 terminé

--- TP5/Player.js
+++ TP5/Player.js
@@ -2,9 +2,8 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 ctx.beginPath();
-ctx.rect(10, 10, 500, 400);
-ctx.strokeStyle = "rgb(0, 0, 255)";
-ctx.stroke();
+ctx.rect(10, 100, 500, 400);
+canvas.style.border = "2px solid black";
 ctx.closePath();
 
 class Player {
@@ -22,19 +21,19 @@ class Player {
     this.level = 1;
 
     //Pas dans update()
-    this.walkSpriteDuration = 5; // Vitesse de l'animation
+    this.walkSpriteDuration = 10; // Vitesse de l'animation
     this.walkSpriteIndex = 0; // Sprite actuel
-    this.walkSpriteNumber = 4;
+    this.walkSpriteNumber = 9;
 
     // Animation Attaque
-    this.attackSpriteDuration = 5;
+    this.attackSpriteDuration = 10;
     this.attackSpriteIndex = 0;
-    this.attackSpriteNumber = 3;
+    this.attackSpriteNumber = 6;
 
     // Animation Mort
     this.dieSpriteDuration = 10;
     this.dieSpriteIndex = 0;
-    this.dieSpriteNumber = 5;
+    this.dieSpriteNumber = 6;
 
     this.idleSpriteIndex = 0;
     this.idleSpriteNumber = 2;
@@ -109,8 +108,12 @@ class Player {
   }
 }
 
+this.isWalking = true;
+this.isAttacking = true;
+this.isDie = true;
+
 let p1 = new Player(1, "poyo", "jsp");
 
-for (let i = 0; i < 15; i++) {
+for (let i = 0; i < 10; i++) {
   p1.animate();
 }

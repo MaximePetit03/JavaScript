f59640332b3bb10665262f7d15c3ce73380bb2fd
MaximePetit03 <Maxime.petit@coda-student.school>
2026-01-13 15:59:25 +0100

    good spritesheet

--- TP5/Player.js
+++ TP5/Player.js
@@ -13,10 +13,10 @@ class Player {
     this.skin = skin;
     this.positionX = 100;
     this.positionY = 100;
+    this.hp = 100;
     this.hpMax = 100;
     this.damage = 10;
     this.heal = 5;
-    this.cooldown = 2000;
     this.speed = 50;
     this.level = 1;
 
@@ -31,8 +31,8 @@ class Player {
     this.attackSpriteNumber = 6;
 
     // Animation Mort
-    this.dieSpriteDuration = 10;
-    this.dieSpriteIndex = 0;
+    this.deathSpriteDuration = 10;
+    this.deathSpriteIndex = 0;
     this.dieSpriteNumber = 6;
 
     this.idleSpriteIndex = 0;
@@ -41,20 +41,21 @@ class Player {
     this.isWalking = false;
     this.isAttacking = false;
     this.isDie = false;
+    this.dead = false;
 
     this.currentWalkSpriteStep = 0;
     this.currentAttackSpriteStep = 0;
-    this.currentDieSpriteStep = 0;
+    this.currentDeathSpriteStep = 0;
     this.currentIdleSpriteStep = 0;
   }
 
   update(updateData) {
     this.positionX = updateData.positionX;
     this.positionY = updateData.positionY;
+    this.hp = updateData.hp;
     this.hpMax = updateData.hpMax;
     this.damage = updateData.damage;
     this.heal = updateData.heal;
-    this.cooldown = updateData.cooldown;
     this.speed = updateData.speed;
     this.level = updateData.level;
   }
@@ -62,36 +63,55 @@ class Player {
   animate() {
     //The player is walking
     if (this.isWalking) {
-      this.currentWalkSpriteStep++;
+      this.walkSpriteIndex = 0;
+      this.currentWalkSpriteStep = 0;
 
+      this.currentWalkSpriteStep++;
       if (this.currentWalkSpriteStep >= this.walkSpriteDuration) {
         this.currentWalkSpriteStep = 0;
-        this.walkSpriteDuration++;
+        this.walkSpriteIndex++;
       }
       if (this.walkSpriteIndex >= this.walkSpriteNumber) {
         this.walkSpriteIndex = 0;
       }
       //The player is attacking
-    } else if (this.isAttacking) {
+    } else if (
+      this.isAttacking ||
+      this.currentAttackSpriteStep > 0 ||
+      this.attackSpriteIndex > 0
+    ) {
       this.currentAttackSpriteStep++;
 
       if (this.currentAttackSpriteStep >= this.attackSpriteDuration) {
         this.currentAttackSpriteStep = 0;
-        this.attackSpriteDuration++;
+        this.attackSpriteIndex++;
       }
       if (this.attackSpriteIndex >= this.attackSpriteNumber) {
         this.attackSpriteIndex = 0;
       }
       //The player is die
-    } else if (this.isDie) {
-      this.currentDieSpriteStep++;
+    } else if (
+      this.isDie ||
+      this.currentDeathSpriteStep > 0 ||
+      this.deathSpriteIndex > 0
+    ) {
+      this.currentDeathSpriteStep++;
 
-      if (this.currentDieSpriteStep >= this.dieSpriteDuration) {
-        this.currentDieSpriteStep = 0;
-        this.dieSpriteDuration++;
+      if (this.currentDeathSpriteStep >= this.deathSpriteDuration) {
+        this.currentDeathSpriteStep = 0;
+        this.deathSpriteIndex++;
+      }
+
+      if (this.currentIdleSpriteStep >= this.idleSpriteNumber) {
+        this.dead = true;
       }
       //The player is idle
     } else {
+      if (this.currentDeathSpriteStep >= this.deathSpriteDuration) {
+        this.currentDeathSpriteStep = 0;
+        this.deathSpriteIndex++;
+      }
+
       if (this.currentIdleSpriteStep >= this.idleSpriteNumber) {
         this.idleSpriteIndex = 0;
       }
